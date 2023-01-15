4c57c06ed492de56e3b958f087098906719aaf64
Rohlik1 <122755658+Rohlik1@users.noreply.github.com>
2023-01-15 23:43:57 +0100

    esx_rpchat
    esx_rpchat

---
+++ esx_rpchat/config.lua
@@ -0,0 +1,24 @@
+Config        = {}
+Config.Locale = 'en'
+
+Config.OnlyICNames = true 
+Config.OnlyFirstname     = true
+Config.EnableTweet       = true
+
+
+
+
+Config.AnnounceMinutes = 30 -- Minuty 
+
+Config.Messages = {
+	{message = "Ahoj. Vítame te zde na serveru Recovery Roleplay. Snad se ti tu bude líbit. Prajem ti pekné RP."},
+}
+
+Config.AnnounceMinutes2 = 30  -- Minuty 
+
+Config.Messages2 = {
+	{message = "Ak stále niesi na našom discorde tak tu je odkaz » dsc.gg/RVRPCZSK"},
+}
+
+
+
